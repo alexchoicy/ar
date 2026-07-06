9552687f14cc8e149feb3e04496df3ce6c23b0e7
alexchoicy <alex123321choicy@gmail.com>
2026-07-06 15:25:55 +0800

    try this azure depoly

--- ar-backend/src/server.ts
+++ ar-backend/src/server.ts
@@ -1,6 +1,7 @@
+import { fileURLToPath } from "node:url";
+
 import express from "express";
 import helmet from "helmet";
-import { fileURLToPath } from "node:url";
 
 import { connectDb } from "./db/connection.js";
 import { errorMiddleware, responseMiddleware } from "./middleware/response.js";
@@ -9,16 +10,22 @@ import { httpLogger, logger } from "./utils/logger.js";
 
 const app = express();
 const port = Number(process.env.PORT) || 3000;
-const publicHtml = fileURLToPath(new URL("../public/Student-documentation.html", import.meta.url));
-const uploadTestHtml = fileURLToPath(new URL("../public/upload-test.html", import.meta.url));
+const publicHtml = fileURLToPath(
+	new URL("../public/Student-documentation.html", import.meta.url),
+);
+const uploadTestHtml = fileURLToPath(
+	new URL("../public/upload-test.html", import.meta.url),
+);
 
 app.disable("x-powered-by");
-app.use(helmet({ contentSecurityPolicy: false }));
+app.use(helmet({ contentSecurityPolicy: false })); //this will be removed after dev
 app.use(express.json());
 app.use(httpLogger);
 app.use(responseMiddleware);
 app.get("/docs", (_request, response) => response.sendFile(publicHtml));
-app.get("/upload-test", (_request, response) => response.sendFile(uploadTestHtml));
+app.get("/upload-test", (_request, response) =>
+	response.sendFile(uploadTestHtml),
+);
 app.use(routes);
 app.use(errorMiddleware);
 
